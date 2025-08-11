d96d3da933d0666ff341d5f828b97339246e38da
Ogik29 <dimasoki1234@gmail.com>
2025-08-11 20:04:08 +0700

    minor change

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -10,13 +10,17 @@ class EventSeeder extends Seeder
     public function run(): void
     {
         Event::create([
-            'name' => 'Turnamen Sepak Bola',
-            'image' => 'event1.jpg',
-            'desc' => 'Kejuaraan sepak bola tahunan.',
-            'kategori' => 'Olahraga',
-            'berkas' => null,
+            'name' => 'Kejuaraan Pencak Silat',
+            'image' => 'poster-kejurcab-pagarnusa.jpg',
+            'desc' => 'Pencak silat merupakan seni beladiri tradisional yang memiliki berbagai ketentuan, keselarasan, keseimbangan, keserasian antara wirama, wirasa, dan wiraga. Juga menanamkan sikap berbudi pekerti yang luhur serta pengamalan falsafah Silat. Pagar Nusa ialah sebuah organisasi yang mempunyai tujuan untuk membentuk suatu wadah dibawah naungan Nahdlatul Ulama yang khusus mengembangkan seni bela diri pencak silat. Di sisi lain tumbuh berbagai perguruan pencak silat dengan segala keanekaragamannya berdasarkan segi agama, aqidah, kepercayaan dan jurus – jurus lainnya. Dalam organisasi ini diharuskan mengajarkan materi jurus baku dari PSNU Pagar Nusa sebagai bukti bahwa perguruan tradisional tersebut tergabung dalam PSNU Pagar Nusa.',
+            'kategori' => 'Link Drive Ketentuan Tanding: https://drive.google.com/drive/folders/1q-vAkN3uUt6wMcYnMBY5y3kCS28_yezF',
+            'berkas' => '- Fotokopi Kartu Keluarga <br>
+                        - Kartu Tanda Anggota Pagar Nusa <br>
+                        - Biodata Atlet <br>
+                        - Formulir Kontingen <br>
+                    ',
             'kegiatan' => 'Pertandingan',
-            'type' => 'Team'
+            'type' => 'kerjasama'
         ]);
     }
 }
